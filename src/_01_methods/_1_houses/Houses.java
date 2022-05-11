dde686ef50c14cbd3160ea1b6e467c9a94b9a17a
student <student@ip-172-31-51-100.us-west-2.compute.internal>
2022-05-10 19:56:20 -0700

    started new level

--- src/_01_methods/_1_houses/Houses.java
+++ src/_01_methods/_1_houses/Houses.java
@@ -1,4 +1,7 @@
 package _01_methods._1_houses;
+
+import org.jointheleague.graphical.robot.Robot;
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2021
  *    Level 1
@@ -7,5 +10,15 @@ public class Houses {
 		
 	public void run() {
 		// Check the recipe to find out what code to put here
+		Robot rob = new Robot();
+		rob.setSpeed(100);
+		rob.moveTo(100, 500);
+		rob.move(100);
+		rob.turn(90);
+		rob.move(10);
+		rob.turn(90);
+		rob.move(100);
+		rob.turn(-90);
+		
 	}
 }

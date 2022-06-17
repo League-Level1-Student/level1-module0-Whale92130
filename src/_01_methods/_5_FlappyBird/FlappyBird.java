6481767621ddaab4325d70a14456228f921b50bf
alexanders <alexanders@DESKTOP-U5OLKBI.san.rr.com>
2022-06-17 13:55:36 -0700

    Finished finishline and list of goal

--- src/_01_methods/_5_FlappyBird/FlappyBird.java
+++ src/_01_methods/_5_FlappyBird/FlappyBird.java
@@ -2,72 +2,76 @@ package _01_methods._5_FlappyBird;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 import processing.core.PApplet;
 
 public class FlappyBird extends PApplet {
-    static final int WIDTH = 800;
-    static final int HEIGHT = 600;
-    int x = 100;
-    int y = 300;
-    int px = 700;
-    int ph = 200;
-    int ph2 = 200;
-    int py = 0;
-    int py2 = 0;
-    int ff = 0;
-    @Override
-    public void settings() {
-        size(WIDTH, HEIGHT);
-    }
+	static final int WIDTH = 800;
+	static final int HEIGHT = 600;
+	int x = 100;
+	int y = 300;
+	int px = 700;
+	int ph = 200;
+	int ph2 = 200;
+	int py = 0;
+	int py2 = 0;
+	int ff = 0;
 
-    @Override
-    public void setup() {
-        
-    }
+	@Override
+	public void settings() {
+		size(WIDTH, HEIGHT);
+	}
 
-    @Override
-    public void draw() {
-    	background(135,206,235);
-    	fill(255,255,0);
-    	stroke(0,0,0);
-    	ellipse(x, y, 100, 100);
-    	if (mousePressed == true) {
-    		for (int er = 0; er < 10; er++) {
-    			y--;
-    		}
-    	}
-    	y++;
-    	fill(0,255,0);
-    	ph2 = 200;
-    	rect(px,py,100,ph);
-    	rect(px,py2,100,300);
-    	px = px - 5;
-    	if (px == -100) {
-    		px = 700;
-    		int ra = new Random().nextInt(200 - 10 + 1) + 100;
-    		py2 = py + ra + 200;
-    		ph = ra;
-    		
-    		
-    	}
-    	if(ff > 200) {
-    	intersectsPipes();
-    	}
-    	ff++;
-    }
+	@Override
+	public void setup() {
 
-    void intersectsPipes() { 
-        if (y < ph && px < 120){
-        	System.out.println("Game Over");
-        	exit();
-        }
-       else if (y>py2 && x > px) {
-    	   System.out.println("Game Over");
-    	   exit();
-       }
-       else {  }
-}
-    static public void main(String[] args) {
-        PApplet.main(FlappyBird.class.getName());
-    }
+	}
+
+	@Override
+	public void draw() {
+		background(135, 206, 235);
+		fill(255, 255, 0);
+		stroke(0, 0, 0);
+		ellipse(x, y, 100, 100);
+		if (mousePressed == true) {
+			for (int er = 0; er < 10; er++) {
+				y--;
+			}
+		}
+		y=y+3;
+		fill(0, 255, 0);
+		ph2 = 200;
+		rect(px, py, 100, ph);
+		rect(px, py2, 100, 300);
+		px = px - 5;
+		if (px == -100) {
+			px = 700;
+			int ra = new Random().nextInt(200 - 10 + 1) + 100;
+			py2 = py + ra + 200;
+			ph = ra;
+
+		}
+		if (ff > 200) {
+			intersectsPipes();
+		}
+		ff++;
+	}
+
+	void intersectsPipes() {
+		if (y < ph && px < 120) {
+			JOptionPane.showMessageDialog(null, "Game Over!");
+			System.out.println("Game Over");
+			exit();
+		} else if (y > py2 && x > px) {
+			JOptionPane.showMessageDialog(null, "Game Over!");
+			System.out.println("Game Over");
+			exit();
+		} else {
+		}
+	}
+
+	static public void main(String[] args) {
+		PApplet.main(FlappyBird.class.getName());
+	}
 }
--- src/_02_return_types/_1_finish_line/FinishLine.java
+++ src/_02_return_types/_1_finish_line/FinishLine.java
@@ -1,12 +1,11 @@
 package _02_return_types._1_finish_line;
 
-
-
 import java.util.Random;
 
 import javax.swing.JOptionPane;
 
 import org.jointheleague.graphical.robot.Robot;
+
 /*
  * HINT: We can tell what a method returns by looking at its return type
  * The return type is always to the left of the method name
@@ -14,49 +13,53 @@ import org.jointheleague.graphical.robot.Robot;
 public class FinishLine {
 	static Robot rob = new Robot("mini");
 	static int totalDistance;
-	
+
 	public static void main(String[] args) {
-		//1. Call the drawFinishLine() method
-		
-		//2. Call the crazyMove() method to move the robot
-		
-		//3. Call the getTotalDistance() and save what is returned into a variable
-		
-		//4. Call the hasCrossedFinishLine() method and save what is returned into a variable
-		
-		//5. If the robot has crossed the finish line... 
-			
-			//6. Use a pop up to say the robot finished and how far it went
-		
-		
-		//7. Else use a pop up to say the robot did not finish and how far it went
-		
+		// 1. Call the drawFinishLine() method
+		drawFinishLine();
+		// 2. Call the crazyMove() method to move the robot
+		crazyMove();
+		// 3. Call the getTotalDistance() and save what is returned into a variable
+		int D = getTotalDistance();
+		// 4. Call the hasCrossedFinishLine() method and save what is returned into a
+		// variable
+		boolean S = hasCrossedFinishLine();
+		// 5. If the robot has crossed the finish line...
+		if (S == true) {
+			JOptionPane.showMessageDialog(null, "The robot finished and traveled " + D + " Pixels");
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "The robot did not fisnish and traveled " + D + " Pixels");
+		}
+		// 6. Use a pop up to say the robot finished and how far it went
+
+		// 7. Else use a pop up to say the robot did not finish and how far it went
+
 	}
-	
+
 	static void crazyMove() {
 		rob.setX(400);
 		rob.setY(600);
-		
-		int numMoves = new Random().nextInt(41)+10;
-		for (int i = 0; i < numMoves; i++ ) { 
+
+		int numMoves = new Random().nextInt(41) + 10;
+		for (int i = 0; i < numMoves; i++) {
 			rob.setAngle(0);
-			if(i%2==0) {
+			if (i % 2 == 0) {
 				rob.turn(45);
-			}
-			else {
+			} else {
 				rob.turn(-45);
 			}
 			int dist = new Random().nextInt(31);
 			rob.move(dist);
-			totalDistance+=dist;
+			totalDistance += dist;
 		}
 	}
-	
+
 	static void drawFinishLine() {
-		rob.setSpeed(100);
+		rob.setSpeed(2000);
 		rob.penDown();
-		
-		//Lines
+
+		// Lines
 		rob.setX(0);
 		rob.setY(200);
 		rob.turn(90);
@@ -64,8 +67,8 @@ public class FinishLine {
 		rob.setY(250);
 		rob.turn(180);
 		rob.move(1000);
-		
-		//F
+
+		// F
 		rob.setX(300);
 		rob.setY(210);
 		rob.turn(180);
@@ -80,8 +83,8 @@ public class FinishLine {
 		rob.move(15);
 		rob.turn(-90);
 		rob.move(15);
-		
-		//I
+
+		// I
 		rob.setX(330);
 		rob.setY(210);
 		rob.turn(-90);
@@ -94,8 +97,8 @@ public class FinishLine {
 		rob.move(10);
 		rob.turn(180);
 		rob.move(20);
-		
-		//N
+
+		// N
 		rob.setX(360);
 		rob.setY(240);
 		rob.turn(90);
@@ -104,8 +107,8 @@ public class FinishLine {
 		rob.move(40);
 		rob.turn(-135);
 		rob.move(30);
-		
-		//I
+
+		// I
 		rob.setX(400);
 		rob.setY(210);
 		rob.turn(90);
@@ -118,8 +121,8 @@ public class FinishLine {
 		rob.move(10);
 		rob.turn(180);
 		rob.move(20);
-		
-		//S
+
+		// S
 		rob.setX(430);
 		rob.turn(180);
 		rob.move(20);
@@ -131,8 +134,8 @@ public class FinishLine {
 		rob.move(15);
 		rob.turn(90);
 		rob.move(20);
-		
-		//H
+
+		// H
 		rob.setX(460);
 		rob.setY(210);
 		rob.turn(90);
@@ -146,16 +149,16 @@ public class FinishLine {
 		rob.turn(180);
 		rob.move(30);
 	}
-	
+
 	static int getTotalDistance() {
 		return totalDistance;
 	}
-	
+
 	static boolean hasCrossedFinishLine() {
-		if(rob.getY() < 200) {
+		if (rob.getY() < 200) {
 			return true;
 		}
 		return false;
 	}
-	
+
 }

6dda861321d258082c9e1b4fb23e33ac287cdbfc
league <league@imac-18.attlocal.net>
2022-07-18 13:59:48 -0700

    Finshed 3 guis

--- src/_03_gui_with_help/_2_photo_quiz/PhotoQuiz.java
+++ src/_03_gui_with_help/_2_photo_quiz/PhotoQuiz.java
@@ -26,33 +26,52 @@ public class PhotoQuiz {
 		// 1. find an image on the internet, and put its URL in a String
 		// variable (from your browser, right click on the image, and select
 		// “Copy Image Address” )
-
+		String image = "https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cmFuZG9tfGVufDB8fDB8fA%3D%3D&w=1000&q=80";
 		// 2. create a variable of type "Component" that will hold your image
-
+		Component imageC;
 		// 3. use the "createImage()" method below to initialize your Component
-
+		imageC = createImage(image);
 		// 4. add the image to the quiz window
-
+		quizWindow.add(imageC);
 		// 5. call the pack() method on the quiz window
-
+		quizWindow.pack();
+		quizWindow.resize(1000, 1000);
 		// 6. ask a question that relates to the image
-
+		String a = JOptionPane.showInputDialog("Is this a banana?");
 		// 7. print "CORRECT" if the user gave the right answer
-
+		if (a.equalsIgnoreCase("Yes")) {
+			System.out.println("Wrong, its a Plantain.");
+		}
+		else {
+			System.out.println("Correct.");
+		}
 		// 8. print "INCORRECT" if the answer is wrong
 
 		// 9. remove the component from the quiz window (you may not see the
 		// effect of this until step 12)
-
+		quizWindow.remove(imageC);
 		// 10. find another image and create it (might take more than one line
 		// of code)
-
+		String image2 = "https://i.pinimg.com/474x/82/22/4a/82224aa00fd121ddb2ce20db2c47c8b1--brain-teasers-perception.jpg";
 		// 11. add the second image to the quiz window
-
+		Component image2C;
+		image2C = createImage(image2);
+		quizWindow.add(image2C);
 		// 12. pack the quiz window
-
+		quizWindow.pack();
+		quizWindow.resize(1000, 1000);
 		// 13. ask another question
-
+		String b = JOptionPane.showInputDialog("Is there a old lady or a young lady?(answer with: old, young, etc)");
+		// 7. print "CORRECT" if the user gave the right answer
+		if (b.equalsIgnoreCase("old")) {
+			System.out.println("Incorrect");	
+		}
+		else if(b.equalsIgnoreCase("both")){
+			System.out.println("Correct.");
+		}
+		else {
+			System.out.println("Incorrect");	
+		}
 		// 14+ check answer, say if correct or incorrect, etc.
 
 	}

175818c7e0e64135fe7b6734c2efb748951f9634
alexanders <alexanders@192.168.1.237>
2022-07-19 19:56:51 -0700

    Finshed fortune cookie and started jack in the box

--- src/_04_gui_from_scratch/_1_fortune_cookie/Fortune_Cookie.java
+++ src/_04_gui_from_scratch/_1_fortune_cookie/Fortune_Cookie.java
@@ -2,9 +2,11 @@ package _04_gui_from_scratch._1_fortune_cookie;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class Fortune_Cookie implements ActionListener {
     public void showButton() {
@@ -16,11 +18,28 @@ public class Fortune_Cookie implements ActionListener {
         frame.add(button);
         frame.pack();
         frame.resize(300,300);
+        
    }
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
+		int rand = new Random().nextInt(5);
+		if(rand == 0) {
+		JOptionPane.showMessageDialog(null, "You will live tmr.");
+		}
+		else if(rand == 1) {
+			JOptionPane.showMessageDialog(null, "You will buy some pants this year.");
+		}
+		else if(rand == 2) {
+			JOptionPane.showMessageDialog(null, "You will make a new friend this week.");
+		}
+		else if(rand == 3) {
+			JOptionPane.showMessageDialog(null, "You will die at the age of 99.");
+		}
+		else if(rand == 4) {
+			JOptionPane.showMessageDialog(null, "You will become an amzing programer");
+		}
 		
 	} 
 }
